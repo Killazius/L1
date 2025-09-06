dad32bd844693ddb4c73eae9e8e3636fba23a015
Максим Мигунов <migunovmaksim44@gmail.com>
2025-09-06 15:25:48 +0300

    l1.9

---
+++ 9/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func gen(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func sq(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
+}
+func main() {
+	for num := range sq(gen(1, 2, 3, 4, 5, 6)) {
+		fmt.Println(num)
+	}
+}
